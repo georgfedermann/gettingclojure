3e4f57d613c8ed880edbe46711a65a7ea6b4f08c
Georg Federmann <georg.federmann@gmail.com>
2020-12-02 21:31:36 +0100

    flatten nested collections

---
+++ prime/README.md
@@ -0,0 +1,26 @@
+# prime
+
+
+## Installation
+
+
+## Usage
+
+Reload the namespace:
+(use 'prime.core :reload)
+
+## Options
+
+
+## Examples
+
+
+### Bugs
+
+
+### Any Other Sections
+### That You Think
+### Might be Useful
+
+## License
+
---
+++ prime/src/prime/core.clj
@@ -0,0 +1,25 @@
+(ns prime.core
+  (:gen-class))
+
+(def book {:title "War and Peace" :author "Leo Tolstoy" :published 1869})
+
+;; flatten a sequence
+(reduce (fn add-element [collection element]
+  (if (sequential? element)
+    (if (not-empty element)
+      (if (sequential? (first element))
+        (add-element collection (first element))
+        (add-element (conj collection (first element)) (rest element)))
+      collection)
+    (conj collection element))) []   '((((:a))))     )
+;; testdata:
+;; '((1 2) 3 [4 [5 6]])
+;; ["a" ["b"] "c"]
+;; '((((:a))))
+
+
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
